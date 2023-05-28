7e03eb1e4d3f2916d93b0f73feee86ce9d1f50e6
ruslanonly <rzaevruslanonly@gmail.com>
2023-05-28 13:48:03 +0300

    Add two indexes;

--- database/data/insert_funcs.sql
+++ database/data/insert_funcs.sql
@@ -1,13 +1,111 @@
 DROP FUNCTION insert_schedule;
-CREATE OR REPLACE FUNCTION insert_schedule() RETURNS VOID AS $$
+
+CREATE OR REPLACE FUNCTION insert_schedule()
+RETURNS VOID AS
+$$
 DECLARE
-counter INT := 1000;
+  i INT := 1;
 BEGIN
+  WHILE i <= 10000 -- Change the number of rows as needed
   LOOP
-    EXIT WHEN counter > 12000;
-    INSERT INTO schedule(route_id, train_id, departure_date, arrival_date)
-    VALUES (6, 5, '999-05-18 15:00:00', CONCAT(counter, '-05-20 19:30:00'));
-  counter := counter + 1;
+    DECLARE
+      departure_date TIMESTAMP;
+      arrival_date TIMESTAMP;
+    BEGIN
+      departure_date := NOW() + INTERVAL '1 day' * FLOOR(RANDOM() * 30); -- Random departure date within the next 30 days
+      arrival_date := departure_date + INTERVAL '1 day' * (FLOOR(RANDOM() * 10) + 1); -- Random arrival date within 1 to 10 days after departure
+    
+      INSERT INTO schedule (route_id, train_id, departure_date, arrival_date, status)
+      VALUES (
+        FLOOR(RANDOM() * 10) + 1, -- Random route_id between 1 and 10
+        FLOOR(RANDOM() * 10) + 1, -- Random train_id between 1 and 10
+        departure_date,
+        arrival_date,
+        CASE FLOOR(RANDOM() * 3)
+          WHEN 0 THEN 'осталось мало мест'
+          WHEN 1 THEN 'есть места'
+          ELSE 'нет мест'
+        END
+      );
+    EXCEPTION
+      WHEN others THEN
+        -- Ignore the exception and continue to the next iteration
+        NULL;
+    END;
+    
+    i := i + 1;
   END LOOP;
+  
+  RETURN;
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
+
+
+
+DROP FUNCTION insert_passenger;
+CREATE OR REPLACE FUNCTION insert_passenger()
+RETURNS VOID AS
+$$
+DECLARE
+  first_names VARCHAR[] := ARRAY['John', 'Emma', 'Michael', 'Sophia', 'William', 'Olivia', 'James', 'Ava', 'Robert', 'Mia'];
+  middle_names VARCHAR[] := ARRAY['Lee', 'Grace', 'Allen', 'Rose', 'David', 'Elizabeth', 'Thomas', 'Victoria', 'Daniel', 'Emily'];
+  last_names VARCHAR[] := ARRAY['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Garcia', 'Miller', 'Davis', 'Wilson', 'Martinez'];
+  i INT := 1;
+BEGIN
+  WHILE i <= 10 -- Change the number of rows as needed
+  LOOP
+    INSERT INTO passenger (first_name, middle_name, last_name, passport_serial_no, passport_code)
+    SELECT
+      first_names[FLOOR(RANDOM() * 10) + 1], -- Random first name from the array
+      middle_names[FLOOR(RANDOM() * 10) + 1], -- Random middle name from the array
+      last_names[FLOOR(RANDOM() * 10) + 1], -- Random last name from the array
+      FLOOR(RANDOM() * 9000) + 1000, -- Random passport_serial_no between 1000 and 9999
+      FLOOR(RANDOM() * 900000) + 100000 -- Random passport_code between 100000 and 999999
+    WHERE NOT EXISTS (
+      SELECT 1 FROM passenger
+      WHERE passport_serial_no = FLOOR(RANDOM() * 9000) + 1000
+        AND passport_code = FLOOR(RANDOM() * 900000) + 100000
+    );
+    
+    i := i + 1;
+  END LOOP;
+
+  RETURN;
+END;
+$$
+LANGUAGE plpgsql;
+
+DROP FUNCTION insert_train;
+CREATE OR REPLACE FUNCTION insert_train()
+RETURNS VOID AS
+$$
+DECLARE
+  i INT := 1;
+BEGIN
+  WHILE i <= 10000 -- Change the number of rows as needed
+  LOOP
+    INSERT INTO train (route_id, name, type, railcar_capacity, railcars_amount, first_class_price, second_class_price)
+    SELECT
+      FLOOR(RANDOM() * 10) + 1, -- Random route_id between 1 and 10
+      'Train ' || i, -- Train name
+      CASE FLOOR(RANDOM() * 3)
+        WHEN 0 THEN 'скоростной-пассажирский'
+        WHEN 1 THEN 'скорый-пассажирский'
+        ELSE 'пассажирский'
+      END, -- Random train type
+      FLOOR(RANDOM() * 200) + 1, -- Random railcar_capacity between 1 and 200
+      FLOOR(RANDOM() * 10) + 1, -- Random railcars_amount between 1 and 10
+      FLOOR(RANDOM() * 1000) + 1, -- Random first_class_price between 1 and 1000
+      FLOOR(RANDOM() * 1000) + 1 -- Random second_class_price between 1 and 1000
+    WHERE NOT EXISTS (
+      SELECT 1 FROM train WHERE name = 'Train ' || i
+    );
+    
+    i := i + 1;
+  END LOOP;
+
+  RETURN;
+END;
+$$
+LANGUAGE plpgsql;
--- database/index.sql
+++ database/index.sql
@@ -1,8 +1,26 @@
-DROP INDEX IF EXISTS idx_station_name;
+DROP INDEX IF EXISTS idx_schedule_departure_date;
+DROP INDEX IF EXISTS idx_btree_schedule_id;
+DROP INDEX IF EXISTS idx_btree_passenger_passport;
+DROP INDEX IF EXISTS idx_route_name;
+DROP INDEX IF EXISTS idx_hash_train_name;
 DROP INDEX IF EXISTS idx_schedule_departure_date;
 
-CREATE INDEX idx_schedule_departure_date ON schedule USING btree(departure_date);
+CREATE INDEX idx_btree_passenger_passport ON passenger USING btree(passport_serial_no, passport_code);
+-- EXPLAIN ANALYZE SELECT id FROM train tr WHERE tr.name = 'Train 9996';
+CREATE INDEX idx_hash_train_name ON train USING hash (name);
+-- EXPLAIN ANALYZE SELECT id FROM passenger WHERE passport_serial_no = 8765 AND passport_code = 135792;
+-- CREATE INDEX idx_schedule_departure_date ON schedule USING gin(departure_date);
 
--- CREATE INDEX idx_passport_serial_no ON passenger USING hash (passport_serial_no);
+-- EXPLAIN ANALYZE SELECT * FROM passenger p WHERE EXISTS (SELECT 1 FROM ticket t INNER JOIN schedule s ON t.schedule_id = s.id WHERE t.passenger_id = p.id AND s.departure_date > CURRENT_TIMESTAMP);
+
+-- EXPLAIN ANALYZE SELECT seats.seat_no
+-- FROM generate_series(1, (SELECT railcar_capacity FROM train WHERE id = (SELECT train_id FROM schedule WHERE id = in_schedule_id))) AS seats(seat_no)
+-- WHERE NOT EXISTS (
+--     SELECT 1
+--     FROM ticket t
+--     WHERE t.schedule_id = in_schedule_id
+--     AND t.railcar_no = in_railcar_no
+--     AND t.seat_no = seats.seat_no
+-- );
 
 -- CREATE INDEX idx_train_type ON train USING schedule(departure_date, arrival_date);
